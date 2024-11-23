42bcdf2e4436f28c55fcb62532333fb9defe6f3f
PrairieWaltz <minnowvswhale@gmail.com>
2024-11-23 14:40:33 -0500

    Fn_revamp

--- dirHunter.sh
+++ dirHunter.sh
@@ -2,52 +2,94 @@
 
 # Check for Dependencies
 for cmd in gobuster ffuf; do
-    if ! command -v $cmd &>/dev/null; then
-        echo "Error: $cmd is not installed. Please install it and try again."
-        exit 1
-    fi
+    command -v $cmd &>/dev/null || { echo "Error: $cmd is not installed. Please install it and try again."; exit 1; }
 done
+
+#Use predefined wordlist or choose your own
+list_finder() {
+    echo -e "\033[32mPlease choose a wordlist:\033[0m"
+    echo "1. Small"
+    echo "2. Big"
+    echo "3. Common"
+    echo "4. Rock You"
+    echo -e "5. Choose your own adventure\n"
+    
+    read -p "Enter your choice (1-5): " choice
+    sleep .75
+    
+    case $choice in
+        1)
+            wordlist="/usr/share/wordlists/dirb/small.txt"
+            echo -e "\n\033[32mYou chose Small.\033[0m"
+        ;;
+        2)
+            wordlist="/usr/share/wordlists/dirb/big.txt"
+            echo -e "\n\033[32mYou chose Big.\033[0m"
+        ;;
+        3)
+            wordlist="/usr/share/wordlists/dirb/common.txt"
+            echo -e "\n\033[32mYou chose Common.\033[0m"
+        ;;
+        4)
+            wordlist="/usr/share/wordlists/rockyou.txt"
+            echo -e "\n\033[32mYou chose Rock You.\033[0m"
+        ;;
+        5)
+            read -p "Enter the full path to your wordlist: " custom_path
+            if [ -f "$custom_path" ]; then
+                wordlist="$custom_path"
+                echo -e "\n\033[32mCustom wordlist set to '$wordlist'.\033[0m"
+            else
+                echo "Error: File not found. Please try again."
+                choose_wordlist
+            fi
+        ;;
+        *)
+            echo "Invalid choice. Please try again.\n"
+            choose_wordlist
+        ;;
+    esac
+}
 
 # Fn to Verify Wordlist
 list_server() {
-    echo -e "\033[32mWould you like to use the same wordlist? (y or n): \033[0m"
+    echo -e "Would you like to use the same wordlist? (y or n): "
     read -r yorn
-    if [ "$yorn" != "y" ]; then
-        echo -e "\nPlease enter the path to your preferred wordlist: "
-        read -r wordlist
-        # Verify if the file exists and is readable
-        if [ ! -f "$wordlist" ] || [ ! -r "$wordlist" ]; then
-            echo -e "\033[31mError: The provided file does not exist or is not readable. Please try again.\033[0m"
-            list_server # Re-prompt for input if invalid
-        else
-            echo -e "\033[32mUsing wordlist: $wordlist\033[0m"
-        fi
-    else
-        echo -e "\033[32mUsing the previously chosen wordlist.\033[0m"
-    fi
+    [ "$yorn" = "y" ] && { echo -e "\n\033[32mUsing the previously chosen wordlist.\n\033[0m"; return; }
+    
+    list_finder
 }
 
+######Start######
+echo -e "\n                        WELCOME TO \033[93m
+DDDD   III  RRRR     H   H  U   U  N   N  TTTTT  EEEE  RRRR
+D   D   I   R   R    H   H  U   U  NN  N    T    E     R   R
+D   D   I   RRRR   - HHHHH  U   U  N N N    T    EEE   RRRR
+D   D   I   R   R    H   H  U   U  N  NN    T    E     R   R
+DDDD   III  R   R    H   H   UUU   N   N    T    EEEE  R   R
+============================================================>
+\033[0m"
+
 # Get URL
-echo -e "\n\033[32mLet's find some sub-Directories.\033[0m\nPlease enter a URL: "
+echo -e "\033[32mLet's find some sub-Directories.\033[0m\nPlease enter a URL: "
 read url
 sleep .75
 
 # Get save path
 saveFolder=SubDirs
-echo -e "\nOutput will be saved at $saveFolder"
+echo -e "\n\033[32mOutput will be saved at $saveFolder\033[0m"
 mkdir -p $saveFolder
 echo -e "Directory $saveFolder created\n"
 sleep .75
 
 # Get Wordlist
-echo -e "\nPlease enter a path to your preferred wordlist: "
-read wordlist
+list_finder
 echo -e "Using $wordlist\n"
-sleep .75
+sleep 1.25
 
 # GOBUSTER
-echo -e "Running gobuster on $url using wordlist at $wordlist\n"
-gobuster dir -u $url -w $wordlist -o $saveFolder/goBusterSubDirs.txt
+echo -e "\033[32mRunning gobuster on $url using wordlist at $wordlist\033[0m\n"
+gobuster dir -u $url -w $wordlist --exclude-length 118 -o $saveFolder/goBusterSubDirs.txt
 echo -e "\n$(date +%b-%d-%Y-%H:%M)" >> $saveFolder/goBusterSubDirs.txt
 echo -e "Gobuster complete. File saved as goBusterSubDirs.txt\n"
 sleep 1
@@ -61,6 +103,7 @@ echo "Running trailing FFUF on $url using wordlist at $wordlist"
 ffuf -u $url/FUZZ -mc all -fc 404 -c -t 50 -w $wordlist -of json -o $saveFolder/ffufTrail.json
 cat $saveFolder/ffufTrail.json | jq -r '.results[] | "\(.status): \(.input.FUZZ)"' > $saveFolder/ffufTrail.txt
 echo -e "\n$(date +%b-%d-%Y-%H:%M)" | tee -a $saveFolder/ffufTrail.json >> $saveFolder/ffufTrail.txt
+
 echo -e "\nTrailing FFUF complete. File(s) saved as ffufTrail.txt\n"
 sleep .25
 
@@ -84,3 +127,5 @@ sleep 1
 
 echo "Thanks. All done"
 exit 0
+
+
